1f887194be6280ec8f9ded4bdbcfe7ffd720ea87
MrFred17 <57257280+MrFred17@users.noreply.github.com>
2020-04-14 23:32:14 +0300

    added another solution

--- solution.py
+++ solution.py
@@ -15,3 +15,12 @@ def valid_parentheses(string):
         return True
     else:
         return False
+
+# this is a pretty similar solution but a bit shorter with less runtime
+def valid_parentheses_v2(string): 
+    counter = 0
+    for char in string:
+        if char == '(': total += 1
+        if char == ')': total -= 1
+        if counter < 0: return False
+    return True if counter == 0 else False
